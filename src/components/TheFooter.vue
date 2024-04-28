73c2c3b34fcdc3bb05b25e5e7571e156327d504b
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-04-28 20:54:42 +0300

    +

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,14 @@
 <template>
- <footer class="">
-  <v-container></v-container>
+ <footer class="bg-[var(--b-dark)]">
+  <v-container>
+   <div class="flex justify-between text-[var(--t-white)] py-[40px]">
+    <div class="flex flex-col gap-[10px]">
+     <a href="#" class="text-30 font-700">ROBO.SCHOOL</a>
+     <span class="text-16 text-[rgba(255,255,255,0.7)]">© ROBO.SCHOOL</span>
+    </div>
+    <a href="tel:+78000001122" class="text-30 font-600">+7 800 000 11 22</a>
+   </div>
+  </v-container>
  </footer>
 </template>
 
