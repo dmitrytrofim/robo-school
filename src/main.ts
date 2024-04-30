6fe54e24629c93a61d24437d24e20b1a52c6dbac
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-04-30 18:40:25 +0300

    +

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import '@/style.css';
 import App from '@/App.vue';
-import components from '@components/index.ts';
+import components from '@components/index';
 import VueSplide from '@splidejs/vue-splide';
 // import router from '@router/router';
 // import { createPinia } from 'pinia';
