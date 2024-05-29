787c0e817f21dc43b53ca9b2f4a5f5075e009618
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-05-29 13:00:10 +0300

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { ViteAliases } from 'vite-aliases';
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base: '/',
+ base: '/portfolio/works/robo-school/',
  plugins: [
   vue(),
   ViteAliases({
